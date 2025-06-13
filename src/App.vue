cda9c214e5389108baf28e57e6e0208f0b304ee8
mohamad-slime <mohamadamin_dastmozdraki@outlook.com>
2025-06-14 00:20:47 +0330

    Refactor App.vue to use script setup and simplify template structure

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,10 @@
-<script>
-  export default {
-    data() {
-      return {
-        name: 'mohamad',
-        status: 'active',
-        tasks: ['task one', 'task two', 'task three', 'task four']
-      }
-    },
-    methods: {
-      changeStatus() {
-        if (this.status == 'active') {
-          this.status = 'inactive'
-        } else if (this.status == 'inactive') {
-          this.status = 'pending'
-        } else {
-          this.status = 'active'
-        }
-      }
-    }
-
-  }
-
+<script setup>
+  import Navbar from '@/components/Navbar.vue';
+  import { RouterView } from 'vue-router';
 
 </script>
 
 <template>
-  <h1>{{ name }}</h1>
-  <p v-if="status == 'active'">User is Active</p>
-  <p v-else-if="status == 'pending'">User is Pending</p>
-  <p v-else>User is Inactive</p>
-  <br>
-  <h3>tasks:</h3>
-  <ul>
-    <li v-for="task in tasks" :key="task">
-      {{ task }}</li>
-  </ul>
-  <br>
-  <button @click="changeStatus">Change Status</button>
+  <Navbar />
+  <RouterView />
 </template>
